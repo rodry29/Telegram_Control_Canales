079ab23fb1543894c26588f81b306691a3a8f25d
rodry29 <54281237+rodry29@users.noreply.github.com>
2026-04-05 19:48:22 -0500

    Update greeting from 'Hello World' to 'Goodbye World'

--- bot.py
+++ bot.py
@@ -16,10 +16,10 @@ from telegram.ext import (
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 # ---------- CONFIGURACIÓN ----------
-TOKEN = os.getenv("8782944509:AAFqTBOCPwJdhRgt2Qxx4Usj45DNF83Y86s")
+TOKEN = "8782944509:AAFqTBOCPwJdhRgt2Qxx4Usj45DNF83Y86s"
 DATABASE_URL = os.getenv("DATABASE_URL")
-ADMIN_ID = int(os.getenv("8682208062", 0))
-VIP_GROUP_ID = int(os.getenv("-1003842587095", 0))
+ADMIN_ID = "8682208062"
+VIP_GROUP_ID = -"1003842587095"
 
 # Planes y precios (días, precio, nombre)
 PLANS = {
